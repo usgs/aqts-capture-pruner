27fa5740c4b582fca0c5beebdf35157994652a08
ssoper <ssoper@usgs.gov>
2020-04-26 22:35:02 -0500

    test for null date

--- src/test/java/gov/usgs/wma/waterdata/PruneTimeSeriesTest.java
+++ src/test/java/gov/usgs/wma/waterdata/PruneTimeSeriesTest.java
@@ -5,11 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-import static gov.usgs.wma.waterdata.PruneTimeSeries.SUCCESS_STATUS;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
+import static gov.usgs.wma.waterdata.PruneTimeSeries.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.NONE)
 public class PruneTimeSeriesTest {
@@ -26,22 +27,23 @@ public class PruneTimeSeriesTest {
 		request.setDate("2020-01-01 10:10:10");
 	}
 
-//	@Test
-//	public void testNotFound() {
-//		when(tsDao.pruneTimeSeries(anyString()).thenReturn(result.set));
-//		ResultObject result = pruneTs.apply(request);
-//		assertNotNull(result);
-//		assertEquals(SUCCESS_STATUS, result.getPruneStatus());
-//	}
+	@Test
+	public void testFound() {
+		when(tsDao.pruneTimeSeries(anyString())).thenReturn(Object.class);
+		ResultObject result = pruneTs.apply(request);
+		assertNotNull(result);
+		assertEquals(SUCCESS_STATUS, result.getPruneStatus());
+	}
 
-//	@Test
-//	public void testFoundGeneric() {
-//		List<TimeSeries> expectedTimeSeriesList = Arrays.asList(
-//				new TimeSeries("uniqueId"),
-//				new TimeSeries("anotherUniqueId"));
-//		when(tsDao.pruneTimeSeries(anyLong())).thenReturn(expectedTimeSeriesList);
-//		ResultObject result = pruneTs.apply(request);
-//		assertNotNull(result);
-//		assertEquals(result.getTimeSeriesList(), expectedTimeSeriesList);
-//	}
+	@Test
+	public void testNullDate() {
+		request.setDate(null);
+		ResultObject result = pruneTs.processRequest(request);
+		assertNotNull(result);
+		assertEquals(FAIL_STATUS, result.getPruneStatus());
+		assertEquals(FAIL_MESSAGE_NULL_DATE, result.getPruneFailMessage());
+		assertThrows(RuntimeException.class, () -> {
+			pruneTs.apply(request);
+		}, "should have thrown an exception but did not");
+	}
 }
