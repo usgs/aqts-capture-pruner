8aade01ac8511f431c27faa69b6e1b90439b9c47
ssoper <ssoper@usgs.gov>
2020-04-28 18:25:52 -0500

    remove spring boot test annotation overhead

--- src/test/java/gov/usgs/wma/waterdata/PruneTimeSeriesTest.java
+++ src/test/java/gov/usgs/wma/waterdata/PruneTimeSeriesTest.java
@@ -2,8 +2,9 @@ package gov.usgs.wma.waterdata;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static gov.usgs.wma.waterdata.PruneTimeSeries.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -12,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.NONE)
+@ExtendWith(SpringExtension.class)
 public class PruneTimeSeriesTest {
 
 	@MockBean
