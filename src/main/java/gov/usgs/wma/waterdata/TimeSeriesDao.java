f7f71ddbc0bb5523494cf85ac96e70e353a383c0
ssoper <ssoper@usgs.gov>
2020-07-06 13:33:45 -0500

    Updated simpleJDBCCall to be compatible with postgres 11

--- src/main/java/gov/usgs/wma/waterdata/TimeSeriesDao.java
+++ src/main/java/gov/usgs/wma/waterdata/TimeSeriesDao.java
@@ -5,11 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.SqlParameter;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 import org.springframework.stereotype.Component;
 
+import java.sql.Types;
 import java.time.LocalDate;
 
 @Component
@@ -31,10 +33,13 @@ public class TimeSeriesDao {
 	public Object pruneTimeSeries(LocalDate date) {
 		Object result = null;
 		try {
+			SqlParameterSource in = new MapSqlParameterSource()
+					.addValue("prunedate", date.minusMonths(monthsToKeep));
 			SimpleJdbcCall simpleJdbcCall = new SimpleJdbcCall(jdbcTemplate)
 					.withFunctionName(PRUNE_TIME_SERIES_DATA_FUNCTION_NAME)
-					.withSchemaName(aqtsSchemaName);
-			SqlParameterSource in = new MapSqlParameterSource().addValue("prunedate", date.minusMonths(monthsToKeep));
+					.withSchemaName(aqtsSchemaName)
+					.withoutProcedureColumnMetaDataAccess()
+					.declareParameters(new SqlParameter("prunedate", Types.DATE));
 			result =  simpleJdbcCall.executeFunction(String.class, in);
 		} catch (Exception e) {
 			// TODO for now, just throw whatever exception we encounter, harden when we have a better idea
